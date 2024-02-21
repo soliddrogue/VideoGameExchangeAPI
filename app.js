af2e23b20f7cdbb3ecb9c814516b833f09e109b3
soliddrogue <sbhattarai@student.neumont.edu>
2024-02-20 19:23:44 -0700

    fixing user session

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Import required modules and libraries
 const express = require("express"); // Express.js framework for building web applications
+const http = require('http');
+const fs = require('fs');
 const app = express(); // Create an instance of the Express application
 const bodyParser = require("body-parser"); // Parse incoming request bodies
 const mongoose = require('mongoose'); // MongoDB object modeling tool for Node.js
@@ -9,7 +11,7 @@ app.use(session({
     secret: '00000', 
     resave: false,
     saveUninitialized: false,
-    cookie: {path : '/',secure: false} 
+    cookie: {path : '/',secure: true} 
   }));
 
 const routes = require("./routes/pages"); // Import the routes defined in the pages module
@@ -52,7 +54,12 @@ db.once('open', () => console.log("Connected to Database"));
 // Set the port for the server to listen on, use the environment variable PORT if available, otherwise default to 3000
 const PORT = process.env.PORT || 3001;
 
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const sslOptions = {
+    key: fs.readFileSync('path/to/your/key.pem'),
+    cert: fs.readFileSync('path/to/your/cert.pem')
+  };
+  
+  // Create an HTTPS server
+  https.createServer(sslOptions, app).listen(3001, () => {
+    console.log(`HTTPS Server is running on port 3001`);
+  });
